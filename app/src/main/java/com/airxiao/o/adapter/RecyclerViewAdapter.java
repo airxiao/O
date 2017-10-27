44918cfdf3665afc83a8370c5750b54b9d41b18a
airxiao <356321639@qq.com>
2017-10-27 23:20:17 +0800

    come on

--- app/src/main/java/com/airxiao/o/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/airxiao/o/adapter/RecyclerViewAdapter.java
@@ -2,8 +2,11 @@ package com.airxiao.o.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.airxiao.o.R;
 import com.airxiao.o.entity.KnowledageResBean;
 
 import java.util.List;
@@ -12,7 +15,7 @@ import java.util.List;
  * Created by xiaoyunlou on 17/10/26.
  */
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private Context mContext;
     private List<KnowledageResBean> list;
@@ -23,17 +26,27 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
         return list.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+        }
     }
 }
